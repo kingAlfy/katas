aad07e42ddc945ca4f4685fe62e0f1fb9f664acc
daw211 <alfonso.ravelo.gil@iesjulianmarias.es>
2023-09-12 18:01:10 +0200

    Java -> TwoSum

---
+++ java/src/main/java/org/katas/TwoSum.java
@@ -0,0 +1,32 @@
+package org.katas;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TwoSum {
+    public static int[] twoSum(int[] numbers, int target) {
+
+        int[] positions = new int[2];
+
+        int cnt = 0;
+        while ( cnt < numbers.length ){
+            for (int j = 0; j < numbers.length; j++){
+                if (cnt == j){
+                    continue;
+                }
+
+                if (numbers[cnt] + numbers[j] == target){
+                    positions[0] = cnt;
+                    positions[1] = j;
+                    cnt = numbers.length+2;
+                    break;
+                }
+            }
+
+            cnt++;
+        }
+        
+        return positions;
+    }
+
+}
